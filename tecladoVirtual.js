767f979bc122bacee2f37524eeb8479ef9d0612c
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-05-19 21:17:24 -0300

    teclado

---
+++ tecladoVirtual.js
@@ -0,0 +1,16 @@
+// function aperta(lt){
+//     document.getElementById("aperta").innerHTML = "Voce apertou a tecla "+lt;
+// }
+
+function aperta(lt){
+    document.getElementById("aperta").innerHTML = `Voce apertou a tecla ${lt}`;
+}
+
+
+function solta(lt){
+    document.getElementById("solta").innerHTML = `Voce apertou a tecla ${lt}`;
+}
+
+
+
+
